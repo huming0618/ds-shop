a011b6c90884f70fe5af57629d42a75e51445d7e
Peter Hu <yesky93@sina.com>
2016-06-26 23:44:35 +0800

    init

---
+++ saleTable.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class saleTable extends React.Component{
+  render(){
+    return <div>Test</div>
+  }
+}
+
+ReactDOM.render(<saleTable/>, document.getElementById('table'));
